dd99a29c70bbd17a20cbf7d53b732d2d91289898
GaryZCLau <garyzclau@gmail.com>
2020-06-09 19:26:13 -0400

    stayloggedin route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get '/users/stay_logged_in', to: 'users#stay_logged_in'
+  post '/users', to: 'users#create'
+  post '/users/login', to: "users#login"
   resources :activities
   resources :friends
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/users', to: 'users#create'
-  post '/users/login', to: "users#login"
 end
