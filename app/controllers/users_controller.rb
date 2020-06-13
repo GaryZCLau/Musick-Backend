9e0cbc58c6d9c9de1b12cc29066e6ffdae284c4a
GaryZCLau <garyzclau@gmail.com>
2020-06-12 21:02:00 -0400

    backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,6 @@ class UsersController < ApplicationController
         render json: @users
     end
 
-    def show
-      @user = User.find(params[:id])
-      render json: @user
-    end
 
     def create
       @user = User.create(params.permit(:name, :password, :image, :status))
